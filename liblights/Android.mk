5fcd907fca7bec739dbcba91713235864416db98
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-02-08 19:37:26 -0600

    Remove check for TARGET_SIMULATOR
    Change-Id: Ie4d82c724b77660d797fe302ba6564faa3c35ed6
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -15,8 +15,6 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-ifneq ($(TARGET_SIMULATOR),true)
-
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
@@ -34,4 +32,3 @@ LOCAL_MODULE := lights.grouper
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif # !TARGET_SIMULATOR
